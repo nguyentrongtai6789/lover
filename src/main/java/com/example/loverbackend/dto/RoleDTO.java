759f46f3de7126e042fea138aac3a5f243c57628
Doan Huu Linh <doanlinh1510@gmail.com>
2024-03-28 19:23:37 +0700

    edit String name => RoleName roleName from RoleDTO

--- src/main/java/com/example/loverbackend/dto/RoleDTO.java
+++ src/main/java/com/example/loverbackend/dto/RoleDTO.java
@@ -1,6 +1,8 @@
 package com.example.loverbackend.dto;
 
+import com.example.loverbackend.model.RoleName;
+
 public class RoleDTO extends BaseDTO {
     private Long id;
-    private String name;
+    private RoleName roleName;
 }
